33aaf619d634420539c6a3d97c6b5640573a261c
joakimkargaard <kim.kargaard@noroff.no>
2021-02-19 11:26:57 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+import ApolloClient, { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+	uri: "https://graphqlzero.almansi.me/api",
+});
+client
+	.query({
+		query: gql`
+			{
+				user(id: 2) {
+					id
+					username
+					email
+					address {
+						geo {
+							lat
+							lng
+						}
+					}
+				}
+			}
+		`,
+	})
+	.then(console.log);
